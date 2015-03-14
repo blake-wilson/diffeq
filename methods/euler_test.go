86f262fe3a35dc40dc08cbe6f1707c3f37dcf52e
blake <bwilson025@gmail.com>
2015-03-13 23:42:35 -0500

    Remove nonsense print statement

--- methods/euler_test.go
+++ methods/euler_test.go
@@ -10,7 +10,6 @@ func TestEuler(t *testing.T) {
 	simpleFunc := func(time float64, val float64) float64 { return 3 * time * time }
 	times, estimates := Euler(simpleFunc, 1, 0, 4, 0.01)
 
-	fmt.Printf("SDFDFSDFS")
 	for i := 0; i < len(times); i++ {
 		fmt.Printf("Value at t=%f is %f\n", times[i], estimates[i])
 	}
