aed3cb27a3719e14efa76656438eba74fadb913d
Blake Wilson <bwilson025@gmail.com>
2015-05-21 21:46:34 -0500

    Fix evaluator test

--- evaluator/eval_test.go
+++ evaluator/eval_test.go
@@ -1,13 +1,14 @@
 package evaluator
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEvalOverInterval(t *testing.T) {
 	var expr = "x + 5"
 	results := EvalOverInterval(expr, "x", 1, 4, 0.5)
-	fmt.Printf("%s", results)
-
+	assert.Equal(t, []float64{6, 6.5, 7, 7.5, 8, 8.5, 9},
+		results)
 }
